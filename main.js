f42e98d6d2743d042172dd7210a95f519aa696d8
Clark A <clark@clarka.me>
2021-12-15 19:13:51 -0600

    init commit

---
+++ main.js
@@ -0,0 +1,27 @@
+/*!
+ * Lesson: One
+ * Description: JavaScript30
+ * Author/Developer: Clark Alford
+ * Author/Developer URL: https://www.clarka.me
+ * Version: v1.0.1
+ * License: The Unlicense: https://unlicense.org
+ */
+
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
